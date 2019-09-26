ccb137e320a981d372917b53fa7a22b765fa97a0
MeilingDai <55425606+MeilingDai@users.noreply.github.com>
2019-09-26 15:25:27 +1000

    Finish question 3

--- script/r_script for practice.R
+++ script/r_script for practice.R
@@ -9,6 +9,7 @@ sessionInfo()
 read_csv("data/BOM_data.csv")
 read_csv("data/BOM_data.csv")
 
+
 #assign csv data as a variable
 BOM_data <- read_csv("data/BOM_data.csv")
 
@@ -39,3 +40,31 @@ question_2 <- BOM_data %>%
  arrange(aver_daily_tem_diff)                                                     #arrange data to find the month saw the lowest average daily temperature difference
 question_2                                                                        #form new data
 write_csv(question_2, "results/question_2.csv")                                   #save new data as csv file and save to folder"Result"
+
+
+#Question3 
+#Which state saw the lowest average daily temperature difference?
+#solution
+read_csv("data/BOM_stations.csv")
+BOM_stations <- read_csv("data/BOM_stations.csv")
+tidy_BOM_stations <- BOM_stations %>%                 
+  gather(Station_number, measurement, -info) %>%                 #tidy data by station number
+  spread(key = info, value = measurement) %>%                     #spread data 
+  mutate(Station_number = as.numeric(Station_number))             #change column name from character to numeric
+tidy_BOM_stations 
+write_csv(tidy_BOM_stations , "results/tidy_BOM_stations .csv")
+
+
+new_BOM_data <- full_join(BOM_data, tidy_BOM_stations)
+new_BOM_data 
+state_temp<- new_BOM_data %>%  separate(col = Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>%   #seperate column of Temp_min_max, into = column "Temp_min" and "Temp_max") 
+  mutate(daily_tem_diff = as.numeric(Temp_max) - as.numeric(Temp_min)) %>%        #creat a new column as daily_tem_diff to calculate daily tem diff    
+  filter(daily_tem_diff != "NA") %>%                                              #filter rows for daily_tem_diff with meaningful numbers
+  group_by(state) %>%                                                             #group data by state
+  summarise(state_daily_tem_diff = mean(daily_tem_diff)) %>%                       #calculate average daily tem diff and assign to a new column
+  arrange(state_daily_tem_diff)      
+state_temp
+write_csv(state_temp, "results/state_temp.csv") 
+          
+
+# Question 4
